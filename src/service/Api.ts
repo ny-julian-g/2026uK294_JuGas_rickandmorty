948f779888480ef9ccaf26a132ca9719432d72f1
ny-julian-g <julian.gasser@noseryoung.ch>
2026-04-02 10:16:48 +0200

    add interceptor, for status 401

--- src/service/Api.ts
+++ src/service/Api.ts
@@ -21,3 +21,14 @@ api.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+api.interceptors.response.use(
+  (response) => response, 
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login"; 
+    }
+    return Promise.reject(error);
+  }
+);
